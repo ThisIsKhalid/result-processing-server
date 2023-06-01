54f78478adc68180bb4fd625c21a3e070408bb1c
ThisIsKhalid <iamkhalid.hs@gmail.com>
2023-06-01 20:38:24 +0600

    successfully deployed in vercel

--- app.js
+++ app.js
@@ -4,10 +4,10 @@ import express from "express";
 const app = express();
 
 // routes
-import courseRoute from "./modules/courses/course.route.js";
-import resultRoute from "./modules/results/result.route.js";
-import studentRoute from "./modules/students/student.route.js";
-import teacherRoute from "./modules/teachers/teachers.route.js";
+import courseRoute from "./src/modules/courses/course.route.js";
+import resultRoute from "./src/modules/results/result.route.js";
+import studentRoute from "./src/modules/students/student.route.js";
+import teacherRoute from "./src/modules/teachers/teachers.route.js";
 
 app.use(cors());
 app.use(express.json());
@@ -19,7 +19,7 @@ app.use("/v1/courses", courseRoute);
 app.use("/v1/teachers", teacherRoute);
 
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+  res.send("Welcome to Result Processing Server site.");
 });
 
 export default app;
--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import dotenv from "dotenv";
 import app from "./app.js";
-import { connectToDatabase } from "./db/db.js";
+import { connectToDatabase } from "./src/db/db.js";
 
 dotenv.config();
 const port = process.env.PORT || 5000;
@@ -10,5 +10,3 @@ await connectToDatabase();
 app.listen(port, () => {
   console.log(`Server is listening on port ${port}`);
 });
-
-
